91d9f8f7215f160632f83da32297a9e98ccd3a6b
Engineer162 <hjaltehansen2008@icloud.com>
2025-09-11 14:28:05 +0200

    added credit where its due

--- shuffle.py
+++ shuffle.py
@@ -1,3 +1,5 @@
+# Substitution key generator written by CREI in python.
+
 import random
 alfabetet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
 random.shuffle(alfabetet)
